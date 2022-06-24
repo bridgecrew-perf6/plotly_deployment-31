c0bcc0323c94d34ef48d2d92422c99e85f61773b
reldrid3 <ross.s.eldridge@gmail.com>
2022-06-24 00:27:36 -0400

    Reorganizing folder structure and updating file locations in code

--- static/js/charts.js
+++ static/js/charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("static/data/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("http://localhost:8000/samples.json").then((data) => {
+  d3.json("http://localhost:8000/static/data/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -61,7 +61,7 @@ function topTenReversed(arr, mapFunction) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("static/data/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     let sampleArray = data.samples;
     let metadataArray = data.metadata;
